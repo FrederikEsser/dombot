f9304b5f6e8f07c80ae069556f83fa4912b0855f
Frederik Esser <fe@skm.dk>
2019-07-02 22:57:05 +0200

    Plaza

--- src/dombot/cards/guilds.cljc
+++ src/dombot/cards/guilds.cljc
@@ -12,4 +12,24 @@
                                   [:give-buys 1]
                                   [:give-coffers 1]]})
 
-(def kingdom-cards [candlestick-maker])
+(defn- plaza-discard-treasure [game {:keys [player-no card-name]}]
+  (cond-> game
+          card-name (push-effect-stack {:player-no player-no
+                                        :effects   [[:discard-from-hand {:card-name card-name}]
+                                                    [:give-coffers 1]]})))
+
+(effects/register {::plaza-discard-treasure plaza-discard-treasure})
+
+(def plaza {:name    :plaza
+            :set     :guilds
+            :types   #{:action}
+            :cost    4
+            :effects [[:draw 1]
+                      [:give-actions 2]
+                      [:give-choice {:text    "You may discard a Treasure for +1 Coffers."
+                                     :choice  ::plaza-discard-treasure
+                                     :options [:player :hand {:type :treasure}]
+                                     :max     1}]]})
+
+(def kingdom-cards [candlestick-maker
+                    plaza])
--- test/dombot/cards/guilds_test.clj
+++ test/dombot/cards/guilds_test.clj
@@ -14,13 +14,42 @@
 (use-fixtures :each fixture)
 
 (deftest candlestick-maker-test
-    (testing "Candlestick Maker"
-      (is (= (-> {:players [{:hand    [candlestick-maker]
-                             :actions 1
-                             :buys    1}]}
-                 (play 0 :candlestick-maker))
-             {:players [{:play-area [candlestick-maker]
-                         :actions   1
-                         :buys      2
+  (testing "Candlestick Maker"
+    (is (= (-> {:players [{:hand    [candlestick-maker]
+                           :actions 1
+                           :buys    1}]}
+               (play 0 :candlestick-maker))
+           {:players [{:play-area [candlestick-maker]
+                       :actions   1
+                       :buys      2
                        :coffers   1}]}))))
 
+(deftest plaza-test
+  (testing "Plaza"
+    (is (= (-> {:players [{:hand    [plaza]
+                           :deck    [estate estate]
+                           :actions 1}]}
+               (play 0 :plaza))
+           {:players [{:hand      [estate]
+                       :play-area [plaza]
+                       :deck      [estate]
+                       :actions   2}]}))
+    (is (= (-> {:players [{:hand    [plaza]
+                           :deck    [copper copper]
+                           :actions 1}]}
+               (play 0 :plaza)
+               (choose nil))
+           {:players [{:hand      [copper]
+                       :play-area [plaza]
+                       :deck      [copper]
+                       :actions   2}]}))
+    (is (= (-> {:players [{:hand    [plaza]
+                           :deck    [copper copper]
+                           :actions 1}]}
+               (play 0 :plaza)
+               (choose :copper))
+           {:players [{:play-area [plaza]
+                       :deck      [copper]
+                       :discard   [copper]
+                       :actions   2
+                       :coffers   1}]}))))
